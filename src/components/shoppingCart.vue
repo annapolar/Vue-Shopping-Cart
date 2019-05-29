e9308a77936d94de5fedb77ae73ddafcf29bfce2
Anna Huang <annatointro@gmail.com>
2019-05-29 16:16:04 -0600

    binding info and adding images from background

--- src/components/shoppingCart.vue
+++ src/components/shoppingCart.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="shoppingCart">
-    <h2>dvd shopping cart</h2>
+    <div class="cards">
+      <div class="card" v-for="movie in movies">
+        <div class="left">
+          <div class="cover" :style="bgcss(movie.cover)"></div>
+        </div>
+        <div class="right">
+          <h2>{{movie.name}}</h2>
+          <h4>{{movie.type}}</h4>
+          <p v-html="movie.description"></p>
+          <div class="price">${{movie.price}}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from "axios"
+import axios from "axios";
 
 export default {
   data() {
@@ -14,14 +26,40 @@ export default {
     };
   },
   created() {
-    axios.get("https://awiclass.monoame.com/api/command.php?type=get&name=movies").then(res => {
-      this.movies = res.data;
-      console.table(this.movies)
-    });
+    axios
+      .get("https://awiclass.monoame.com/api/command.php?type=get&name=movies")
+      .then(res => {
+        this.movies = res.data;
+        console.table(this.movies);
+      });
+  },
+  methods: {
+    bgcss(url) {
+      return{
+        'background-image':'url('+ url +')',
+        'background-position':'center center',
+        'background-size':'cover'
+      }
+    }
   }
 };
 </script>
 
 
 <style scoped lang="scss">
+.shoppingCart {
+  .cards {
+   .card{
+     .left{
+       .cover{
+         width:180px;
+         height:300px;
+       }
+     }
+     .right{
+
+     }
+   }
+  }
+}
 </style>
