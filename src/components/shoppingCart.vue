aa9f6e8de22d6da6ec326ed26d6e2ca8b6630660
Anna Huang <annatointro@gmail.com>
2019-05-29 15:15:03 -0600

    first commit

---
+++ src/components/shoppingCart.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="shoppingCart">
+    <h2>dvd shopping cart</h2>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style scoped lang="scss">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import shoppingCart from './components/shoppingCart.vue'
 
 Vue.config.productionTip = false
+Vue.component('shoppingCart',shoppingCart)
 
 new Vue({
   render: h => h(App),
